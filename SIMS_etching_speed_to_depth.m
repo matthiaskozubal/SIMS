2c7665a709a64c782a3aed993020d5bb0c72c1d2
matthiasphysicists <matthiasphysicists@gmail.com>
2015-11-19 12:35:00 +0100

    Minor changes
    description, spaces, etc.

--- SIMS_etching_speed_to_depth.m
+++ SIMS_etching_speed_to_depth.m
@@ -1,4 +1,4 @@
-% Changes SIMS speed of etching to depth
+% Takes results of Secondary Ion Mass Spectroscopy (SIMS) measurements. Changes x-axis from speed of etching to depth
 %% Input
 mk_Clear
 cd('c:\Users\DUIN\Downloads');
@@ -7,7 +7,6 @@ interface_planned_um   = [4.5, 6.1228, 11.6228]; %[4.5, 6.1228, 11.6228]; %[4.5,
 interface_measured_sims  = [635.7, 895, 1635]; %[1056.6, 1493.5, 2756]; %[618.04, 883.04, 1342.77]; %[ 722.77, 1016.6, 1847.5]; % measured depths of interfaces (arb. u.)
 
 %% Calc
-
 % check
 if length(interface_planned_um) ~= length(interface_measured_sims)
     error('Lengths of interface vectors must be equal.')
@@ -23,7 +22,7 @@ end
 scaling_factor = zeros(length(interface_planned_um)-1,1);
 x_scaled = [];
 
-% factors
+% scalling factors
 for i=1:1:(length(interface_planned_um)-1)
     scaling_factor(i) = (interface_planned_um(i+1) - interface_planned_um(i)) / (interface_measured_sims(i+1) - interface_measured_sims(i));
 end
@@ -37,7 +36,7 @@ for i=1:1:(length(interface_planned_um)-1)
     end
 end
 
-% save
+%% save
 dlmwrite('x_scaled.txt', x_scaled);
 
     
